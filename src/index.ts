c26c5b78fa17a65df620b0e642c958aba4fa644e
Erika Freitas <erikamsfreitas@gmail.com>
2022-07-17 16:31:35 -0300

    add Users CRUD endpoints and Status endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'update' });
-});
-
+// Server
 app.listen(3000, () => {
   console.log('App running on port 3000');
 });
+
+// App config (global middlewares)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(statusRoute);
+app.use(usersRoute);
