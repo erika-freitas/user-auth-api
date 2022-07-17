a9727029fd1e575aaa3768bd2d16a072ed14faa6
Erika Freitas <erikamsfreitas@gmail.com>
2022-07-17 14:06:57 -0300

    Create a server with Express and optimize development

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'update' });
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
